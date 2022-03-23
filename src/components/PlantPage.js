964bd173481c2460f2d710599674f6ce80dcf6b8
Noah Reece <noah.w.reece@gmail.com>
2022-03-23 09:49:19 -0400

    Deliverables 1 and 2 met

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  
+  useEffect(() => {
+    fetch(`http://localhost:6001/plants`)
+      .then( res => res.json())
+      .then( data => setPlants(data))
+      .catch( error => alert(error.message));
+  }, [])
+
+  const addPlant = (newPlantData) => {
+    fetch(`http://localhost:6001/plants`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(newPlantData)
+    })
+      .then( res => res.json())
+      .then( newPlant => setPlants([...plants, newPlant]))
+      .catch( error => alert(error.message));
+  }
+  
+  // const updatePlant = (updatedPlantData) => {
+  //   fetch(`http://localhost:6001/plants/${updatedPlantData.id}`, {
+  //     method: "PATCH",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json"
+  //     },
+  //     body: JSON.stringify(updatedPlantData)
+  //   })
+  //     .then( res => res.json())
+  //     .then( updatedPlant => {
+  //       const updatedPlants = plants.map( plant => {
+  //         return plant.id === updatedPlant.id ? updatedPlant : plant;
+  //       })
+  //       setPlants(updatedPlants);
+  //     })
+  //     .catch( error => alert(error.message));
+  // }
+  
+  // const removePlant = (plantToRemove) => {
+  //   fetch(`http://localhost:6001/plants/${plantToRemove.id}`, {
+  //     method: "DELETE",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json"
+  //     }
+  //   })
+  //     .then( res => {
+  //       if (res.ok) {
+  //         const updatedPlants = plants.filter( plant => {
+  //           return plant.id !== plantToRemove.id;
+  //         })
+  //         setPlants(updatedPlants);
+  //       } else {
+  //         alert('something went wrong');
+  //       }
+  //     })
+  //     .catch( error => alert(error.message))
+  // }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm addPlant={addPlant} />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
